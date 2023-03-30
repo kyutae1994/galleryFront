34d479ac720a60802cd091eed30ab22af6f48e40
kyutae1994 <k305059ute@gmail.com>
2023-03-30 16:58:48 +0900

    db 세팅 및 상품 진열 로직 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "@/pages/Home.vue";
+import Login from "@/pages/login.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    {path:'/', component: Home},  // component를 Home으로 연결을 해라
+    {path:'/login', component: Login}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
