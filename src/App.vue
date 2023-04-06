a639b76597a3b4857768013f4ec68d5b948bb4b1
kyutae1994 <k305059ute@gmail.com>
2023-04-06 16:26:30 +0900

    jwt 로그인

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import store from "@/scripts/store";
+import axios from "axios";
+import {useRoute} from "vue-router";
+import {watch} from "vue";
 
 export default {
   name: 'App',
@@ -15,12 +18,19 @@ export default {
     Footer,
     Header
   },
-  setup(){
-    const id = sessionStorage.getItem("id");
+  setup() {
+    const check = () => {
+      axios.get("/api/account/check").then(({data}) => {
+        console.log(data);
+        store.commit("setAccount", data || 0); // 데이터 값이 있으면 data 넣어주고 없으면 0
+      })
+    };
 
-    if (id) {
-      store.commit("setAccount", id);
-    }
+    const route = useRoute();
+
+    watch(route, () => { // 수가 바뀔때마다 check 실행(감시)
+      check();
+    })
   }
 }
 </script>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -3,7 +3,7 @@ import Login from "@/pages/login.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
-    {path:'/', component: Home},  // component를 Home으로 연결을 해라
+    {path:'/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
     {path:'/login', component: Login}
 ]
 
