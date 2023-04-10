a642b21371e02bf50673586dc5e68ebe9850687b
kyutae1994 <k305059ute@gmail.com>
2023-04-10 23:52:08 +0900

    장바구니 기능 마무리

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,19 +1,87 @@
 <template>
   <div class="cart">
-    카트 페이지
+    <div class="container">
+      <ul>
+        <li v-for="(i, idx) in state.items" :key="idx">
+          <img :src="i.imgPath"/>
+          <span class="name">{{ i.name }}</span>
+          <span class="price">{{ lib.getNumberFormatted(i.price - i.price * i.discountPer / 100) }}</span>
+          <i class="fa fa-trash" @click="remove(i.id)"></i>
+        </li>
+      </ul>
+      <router-link to="/order" class="btn btn-primary">구입하기</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import {reactive} from "vue";
+import lib from "@/scripts/lib";
 
 export default {
   setup() {
-    axios.get("/api/cart/items").then((res) => {
-      console.log(res.data);
+    const state = reactive({
+      items: []
     })
+
+    const load = () => {
+      axios.get("/api/cart/items").then((res) => {
+        console.log(res.data);
+        state.items = res.data;
+      })
+    };
+
+    const remove = (itemId) => {
+      axios.delete(`/api/cart/items/${itemId}`).then(() => {
+        load();
+      })
+    }
+
+    load();
+
+    return {state, lib, remove}
   }
 }
 </script>
 <style scoped>
+.cart ul {
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+}
+
+.cart ul li {
+  border: 1px solid #eee;
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+
+.cart ul li img {
+  width: 150px;
+  height: 150px;
+}
+
+.cart ul li .name {
+  margin-left: 25px;
+}
+
+.cart ul li .price {
+  margin-left: 25px;
+}
+
+.cart ul li i {
+  float: right;
+  font-size: 20px;
+  margin-top: 65px;
+  margin-right: 50px;
+}
+
+.cart .btn {
+  width: 300px;
+  display: block;
+  margin: 0 auto;
+  padding: 30px 50px;
+  font-size: 20px;
+}
 </style>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,12 +1,14 @@
 import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
 import Cart from "@/pages/Cart.vue";
+import Order from "@/pages/Order";
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
-    {path:'/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
-    {path:'/login', component: Login},
-    {path:'/cart', component: Cart}
+    {path: '/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
+    {path: '/login', component: Login},
+    {path: '/cart', component: Cart},
+    {path: '/order', component: Order}
 ]
 
 const router = createRouter({
