e27e5fc076646f700bf5f14911c995964b47ada6
kyutae1994 <k305059ute@gmail.com>
2023-04-10 16:55:22 +0900

    장바구니 기능 구현

---
+++ src/pages/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cart">
+    카트 페이지
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  setup() {
+    axios.get("/api/cart/items").then((res) => {
+      console.log(res.data);
+    })
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,10 +1,12 @@
 import Home from "@/pages/Home.vue";
-import Login from "@/pages/login.vue";
+import Login from "@/pages/Login.vue";
+import Cart from "@/pages/Cart.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {path:'/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
-    {path:'/login', component: Login}
+    {path:'/login', component: Login},
+    {path:'/cart', component: Cart}
 ]
 
 const router = createRouter({
