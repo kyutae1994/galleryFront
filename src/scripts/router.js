1fbd485d6825b78825201dd09ed27bc8f835e8d3
kyutae1994 <k305059ute@gmail.com>
2023-04-11 16:35:40 +0900

    주문 기능 구현

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -3,12 +3,14 @@ import Login from "@/pages/Login.vue";
 import Cart from "@/pages/Cart.vue";
 import Order from "@/pages/Order";
 import {createRouter, createWebHistory} from "vue-router";
+import Orders from "@/pages/Orders.vue";
 
 const routes = [
     {path: '/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
     {path: '/login', component: Login},
     {path: '/cart', component: Cart},
-    {path: '/order', component: Order}
+    {path: '/order', component: Order},
+    {path: '/orders', component: Orders}
 ]
 
 const router = createRouter({
