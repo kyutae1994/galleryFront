0c44bfbad6a8e1f5b1ee75a42d8bd4475bf70020
kyutae1994 <k305059ute@gmail.com>
2023-04-17 17:14:49 +0900

    회원가입 구현

---
+++ src/pages/Register.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="form-signin w-100 m-auto">
+    <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="example@example.com"
+             @keyup.enter="submit()" v-model="state.form.email">
+      <label for="floatingInput">Email address</label>
+      <div v-if="idValid">
+        중복된 email 입니다.
+      </div>
+    </div>
+    <button class="w-100 btn btn-sm btn-primary" @click="duplicate()">Duplicate check</button>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+             @keyup.enter="submit()" v-model="state.form.password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign up</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+import axios from "axios";
+import store from "@/scripts/store";
+import router from "@/scripts/router";
+
+export default {
+  setup() {
+    const state = reactive({
+      form: {
+        email: "",
+        password: ""
+      }
+    })
+
+    const submit = () => {
+      axios.post("/api/account/login", state.form).then((res) => {
+        store.commit('setAccount', res.data);
+        sessionStorage.setItem("id", res.data);
+        router.push({path: "/"});
+        window.alert('로그인 되었습니다.');
+      }).catch(()=>{
+        window.alert("로그인 정보가 존재하지 않습니다.");
+      });
+    }
+
+    const duplicate = () => {
+      axios.post("/api/account/duplicate", state.form).then((res) => {
+        let check = res.data;
+        window.alert(check);
+        return check;
+      })
+    }
+
+    return {state, submit, duplicate}
+  }
+}
+</script>
+<style scoped>
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -4,13 +4,15 @@ import Cart from "@/pages/Cart.vue";
 import Order from "@/pages/Order";
 import {createRouter, createWebHistory} from "vue-router";
 import Orders from "@/pages/Orders.vue";
+import Register from "@/pages/Register.vue";
 
 const routes = [
     {path: '/', component: Home},  // 루트 경로로 들어오면 component를 Home으로 연결을 해라
     {path: '/login', component: Login},
     {path: '/cart', component: Cart},
     {path: '/order', component: Order},
-    {path: '/orders', component: Orders}
+    {path: '/orders', component: Orders},
+    {path: '/register', component: Register}
 ]
 
 const router = createRouter({
